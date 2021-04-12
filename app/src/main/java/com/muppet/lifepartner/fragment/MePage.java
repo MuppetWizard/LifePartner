0eb181edd2b401ecc59983f1666974f72398d28a
muppet <811583783@qq.com>
2021-04-12 18:17:48 +0800

    add bannerAd & FullScreen Video ad

--- app/src/main/java/com/muppet/lifepartner/fragment/MePage.java
+++ app/src/main/java/com/muppet/lifepartner/fragment/MePage.java
@@ -12,6 +12,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -20,9 +21,13 @@ import com.muppet.lifepartner.activity.ActAboutUs;
 import com.muppet.lifepartner.activity.ActMyFeadback;
 import com.muppet.lifepartner.activity.ActWebview;
 import com.muppet.lifepartner.util.Constant;
+import com.youyi.yesdk.ad.BannerAd;
+import com.youyi.yesdk.ad.FullVideoAd;
 import com.youyi.yesdk.ad.RewardVideoAd;
-import com.youyi.yesdk.business.UERewardManager;
+import com.youyi.yesdk.business.UEAdManager;
 import com.youyi.yesdk.business.YOUEAdConstants;
+import com.youyi.yesdk.listener.BannerAdListener;
+import com.youyi.yesdk.listener.FullVideoListener;
 import com.youyi.yesdk.listener.RewardListener;
 
 import org.jetbrains.annotations.Nullable;
@@ -50,21 +55,84 @@ public class MePage extends SupportFragment {
     LinearLayout llRewardCsj;
     @BindView(R.id.ll_reward_gdt)
     LinearLayout llRewardGdt;
+    @BindView(R.id.banner)
+    FrameLayout flBanner;
+    @BindView(R.id.ll_full_video)
+    LinearLayout llFullVideo;
 
-
+    private BannerAd bannerAd;
+    private FullVideoAd fullVideoAd;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = LayoutInflater.from(getContext()).inflate(R.layout.me_page, null);
         ButterKnife.bind(this,view);
-
+        loadBanner();
         return view;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        bannerAd.destroy();
+
+    }
+
+    private void loadBanner() {
+        bannerAd = new BannerAd();
+        bannerAd.setBannerConfig(getActivity(),"0000000040",500,108,false);
+        bannerAd.loadAdBanner(flBanner, new BannerAdListener() {
+            @Override
+            public void onError(@Nullable Integer integer, @Nullable String s) {
+                Log.e(Constant.TAG,"code:"+integer+" msg: "+ s);
+            }
+
+            @Override
+            public void onShow() {
+                Log.e(Constant.TAG,"onShow");
+            }
+
+            @Override
+            public void onClosed() {
+                Log.e(Constant.TAG,"onClosed");
+            }
+
+            @Override
+            public void onClicked() {
+                Log.e(Constant.TAG,"onClicked");
+            }
+
+            @Override
+            public void onShowAdOverLay() {
+                Log.e(Constant.TAG,"onShowAdOverLay");
+            }
+
+            @Override
+            public void onAdCloseOverLay() {
+                Log.e(Constant.TAG,"onAdCloseOverLay");
+            }
+
+            @Override
+            public void onDislikeShow() {
+                Log.e(Constant.TAG,"onDislikeShow");
+            }
+
+            @Override
+            public void onDislikeSelected(int i, @Nullable String s) {
+                Log.e(Constant.TAG,"onDislikeSelected");
+            }
+
+            @Override
+            public void onDislikeCanceled() {
+                Log.e(Constant.TAG,"onDislikeCanceled");
+            }
+        });
     }
 
     private void loadRewardVideo(String id) {
         RewardVideoAd ad = new RewardVideoAd();
-        ad.setRewardConfig(getActivity(),new UERewardManager()
+        ad.setRewardConfig(getActivity(),new UEAdManager()
 //                .setUserID("youe_TEST")
 //                .setCustomData("youe_data")
                 .setExpressViewAcceptedSize(500f,500f)
@@ -121,7 +189,61 @@ public class MePage extends SupportFragment {
 
     }
 
-    @OnClick({R.id.my_about_as, R.id.my_feedback, R.id.protocol,R.id.ll_reward_csj,R.id.ll_reward_gdt})
+    private void loadFullScreenVideo(String id) {
+        fullVideoAd = new FullVideoAd();
+        fullVideoAd.setVideoConfig(getActivity(),
+                new UEAdManager()
+                        .setExpressViewAcceptedSize(500,500)
+                        .setOrientation(YOUEAdConstants.VERTICAL)
+                        .setMinVideoDuration(5)
+                        .setMaxVideoDuration(61)
+                        .build());
+        fullVideoAd.loadFullVideo(id, new FullVideoListener() {
+            @Override
+            public void onError(@Nullable Integer integer, @Nullable String s) {
+                Log.d(Constant.TAG,"onError : code: "+ integer+" msg : "+ s);
+            }
+
+            @Override
+            public void onAdLoaded() {
+                Log.d(Constant.TAG,"onAdLoaded");
+            }
+
+            @Override
+            public void onAdCached() {
+                Log.d(Constant.TAG,"onAdCached");
+                fullVideoAd.show();
+            }
+
+            @Override
+            public void onAdShow() {
+                Log.d(Constant.TAG,"onAdShow");
+            }
+
+            @Override
+            public void onAdSkipped() {
+                Log.d(Constant.TAG,"onAdSkipped");
+            }
+
+            @Override
+            public void onAdClicked() {
+                Log.d(Constant.TAG,"onAdClicked");
+            }
+
+            @Override
+            public void onAdComplete() {
+                Log.d(Constant.TAG,"onAdComplete");
+            }
+
+            @Override
+            public void onAdClosed() {
+                Log.d(Constant.TAG,"onAdClosed");
+            }
+        });
+    }
+
+    @OnClick({R.id.my_about_as, R.id.my_feedback, R.id.protocol,R.id.ll_reward_csj,
+            R.id.ll_reward_gdt,R.id.ll_full_video})
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.my_about_as:
@@ -141,6 +263,10 @@ public class MePage extends SupportFragment {
             case R.id.ll_reward_gdt:
                 loadRewardVideo("0000000033");
                 break;
+            case R.id.ll_full_video:
+                loadFullScreenVideo("0000000046");
         }
     }
+
+
 }
